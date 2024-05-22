6ef9ff2aa85e51c0643589021534322170d15a9c
Rafil-Lakani <lakhanirafik111@gmail.com>
2024-05-22 22:52:54 +0530

    add new feture

---
+++ script.js
@@ -0,0 +1,248 @@
+function openlogin(){
+    document.querySelector('.login-page').style.display="flex";
+  closeregister();
+  closemenu();
+}
+
+
+
+function closelogin(){
+    document.querySelector('.login-page').style.display="none";
+    console.log("login cls");
+}
+
+
+function openregister(){
+    document.querySelector('.register-page').style.display="flex";
+   
+}
+
+
+function closeregister(){
+    document.querySelector('.register-page').style.display="none";
+    
+}
+
+function registeruser(){
+    if(true){
+        const usernm= document.querySelector("#username");
+        const phonenm=document.querySelector("#phonenm");
+        const useremail=document.querySelector("#useremail");
+        const password=document.querySelector("#password");
+        
+        localStorage.setItem("usernm",usernm.value);
+        localStorage.setItem("phonenm",phonenm.value);
+        localStorage.setItem("useremail",useremail.value);
+        localStorage.setItem("password",password.value);
+        gotoindex();
+        alert("succesfully register");
+        
+
+    }
+    else{
+        alert("enter all detail");
+    }
+
+}
+
+function loginuser(){
+    const lngemail=document.querySelector("#lnguseremail");
+    const lngpass=document.querySelector("#lngpass");
+    
+    
+    
+    if(lngemail.value==localStorage.getItem("useremail") && lngpass.value==localStorage.getItem("password")){
+        gotoindex();
+        alert("successfully login");
+    }
+    else{ alert("invailide username");}
+}
+
+function gotoindex(){
+    closelogin();
+    closeregister();
+   const logo= document.getElementById("logo-svg")
+   logo.classList.remove("ri-user-3-line");
+   logo.classList.add("ri-account-pin-circle-fill");
+
+
+}
+
+
+// here menu code
+const menudiv=document.querySelector(".menu-div");
+function menu(){
+    menudiv.style.right="0%";
+}
+
+function closemenu(){
+    menudiv.style.right="-100%";
+}
+
+// name changing animation start frome here
+
+const nm = document.querySelector('#change-item');
+
+
+const bagTypes = ['office bags.', 'travelling bags.', 'trolley bags.', 'school bags.'];
+let i = 0;
+function changeAnimation() {
+
+    nm.textContent = bagTypes[i];
+    i++;
+
+    if (i === bagTypes.length) {
+        i = 0;
+    }
+
+    setTimeout(changeAnimation, 5000);
+
+}
+
+changeAnimation();
+
+// silde animation 
+
+var currantindex = 0;
+const bags = document.querySelectorAll('.bags');
+console.log(bags);
+
+
+bags.forEach( bags=> {
+    bags.style.display='none';
+});
+
+bags[currantindex].style.display='flex';
+
+document.querySelector("#left").addEventListener("click", function prv() {
+    if (currantindex > 0) {
+        bags[currantindex].style.display = "none";
+        currantindex--;
+        bags[currantindex].style.display = "flex";
+       
+    }
+});
+document.querySelector("#right").addEventListener("click",function next() {
+    if (currantindex <3) {
+        bags[currantindex].style.display = "none";
+        currantindex++;
+        bags[currantindex].style.display = "flex";
+     
+    }
+})
+
+
+
+// here add card start now 
+
+const cartdbtn =document.querySelector(".addcard" );
+function addcard(pid,count=1)
+ {
+    localStorage.setItem(`${pid}`,`${count}`);
+    console.log(pid);
+    console.log(count);
+
+
+    cartdbtn.style.backgroundColor="var(--text-color-dark)"
+    cartdbtn.style.color="--text-color-light"
+
+   setTimeout(()=>{
+    console.log("funtion run");
+    cartdbtn.style.backgroundColor="var(--back-color2)"
+    cartdbtn.style.color="--text-color"
+   },200)
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// gsap animation start from here
+
+const tl =gsap.timeline();
+
+
+tl.from(".left-nav",{
+    y:"-100%",
+    duration:0.5
+})
+
+tl.from(".center-nav h5",{
+    y:"-300%",
+    duration:0.7
+},"=0.10")
+
+tl.from(".right-nav",{
+    y:"-100%",
+    duration:0.5
+})
+
+
+gsap.from(".left-center",{
+    x:"-200%",
+    duration:1.2,
+    delay:0.2
+  
+})
+
+gsap.from(".heading-sec",{
+    x:-300,
+    duration:1,
+    scrollTrigger:{
+        trigger:".heading-sec",
+        start:"top: 700",
+        scroll:"body"
+    }
+})
+
+gsap.from(".detail-office",{
+    x:-600,
+    duration:1.2,
+    scrollTrigger:{
+        scroll:"body",
+        trigger:".detail-office",
+        start:"top: 620"
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
